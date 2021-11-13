00fadf80f8eb2a550452ae2f06b9e195f5485dbe
shipra1230 <94232093+shipra1230@users.noreply.github.com>
2021-11-13 20:56:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+
+function updateMap() {
+    console.log("Updating Map with real-time Data")
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            //console.log(rsp.data)
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+
+               cases= element.infected;
+               if (cases>255){
+                   color="rgb(255,0,0)";
+               }
+               else {
+                   color=`rgb(${cases},0,0)`;
+               }
+                //Mark on the map
+                new mapboxgl.Marker({
+                    darggable: false,
+                    color:color
+                }).setLngLat([longitude, latitude])
+                .addTo(map);
+            });
+        })
+}
+let interval=2000;
+setInterval( updateMap, interval);
